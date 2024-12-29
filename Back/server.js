7b0086a15705f3f44a455149261471f8a117ed8a
udK01 <dominik.muranyi@gmail.com>
2024-12-29 14:29:32 +0000

    Linked Web Socket Server To Front End.

--- Back/server.js
+++ Back/server.js
@@ -1,1 +1,11 @@
 const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected!");
+
+  ws.on("close", () => {
+    console.log("Client has disconnected.");
+  });
+});
--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,3 +1,9 @@
 export default function App() {
+  const ws = new WebSocket("ws://localhost:8080");
+
+  ws.addEventListener("open", () => {
+    console.log("We are connected!");
+  });
+
   return <div className="text-[32px] text-orange-500">Hello</div>;
 }
