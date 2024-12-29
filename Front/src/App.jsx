226787e2ae81f1482f9af31216f644abf5a08d45
udK01 <dominik.muranyi@gmail.com>
2024-12-29 16:46:15 +0000

    Added Basic UI.

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -11,5 +11,21 @@ export default function App() {
     console.log(data);
   });
 
-  return <div className="text-[32px] text-orange-500">Hello</div>;
+  return (
+    <section className="h-screen bg-slate-600 flex flex-col justify-center items-center">
+      <img src="./logo.png" className="size-[120px]" />
+      <div className="flex w-[40%] h-[60%]">
+        <div className="w-[25%] bg-slate-700 ring-4 ring-slate-800 rounded-md">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+        <div className="w-[75%] bg-slate-700 ring-4 ring-slate-800 rounded-md">
+          <div>Chat</div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ['"EB Garamond"', "serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
