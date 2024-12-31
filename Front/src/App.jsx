676badd51ba14522fa96b1766518f21f23feca74
udK01 <dominik.muranyi@gmail.com>
2024-12-31 19:20:00 +0000

    Compartmentalised Chat Log.

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,44 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-
-import { useWebSocket } from "./WebSocketProvider";
-import { useUser } from "./UserProvider";
+import { useState } from "react";
 
 import User from "./Components/User";
+import Log from "./Components/Log";
+
+import { useWebSocket } from "./WebSocketProvider";
 
 export default function App() {
-  const { profilePicture } = useUser();
-  const { ws } = useWebSocket();
-
   const [message, setMessage] = useState("");
-  const [messageData, setMessageData] = useState([]);
-  const messagesEndRef = useRef(null);
 
-  // History - Message Updater.
-  useEffect(() => {
-    if (ws) {
-      const handleMessage = ({ data }) => {
-        const parsedData = JSON.parse(data);
-        if (parsedData.type === "history") {
-          setMessageData(parsedData.messages);
-        } else {
-          setMessageData((prevMessages) => [...prevMessages, parsedData.data]);
-        }
-      };
-
-      ws.addEventListener("message", handleMessage);
-
-      return () => {
-        ws.removeEventListener("message", handleMessage);
-      };
-    }
-  }, [ws]);
-
-  // Smooth Scroll
-  useEffect(() => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [messageData]);
+  const { ws } = useWebSocket();
 
   // Message Handler
   const handleSendMessage = () => {
@@ -61,27 +31,7 @@ export default function App() {
           <User />
         </div>
         <div className="w-[70%] bg-slate-700 ring-4 ring-slate-800 rounded-md">
-          <div className="w-full h-[90%]">
-            <div className="p-4 text-white h-full overflow-y-auto scrollbar-thin scrollbar-thumb-slate-900 scrollbar-track-transparent">
-              {messageData
-                .filter((msg) => msg !== undefined)
-                .map((msg, index) => (
-                  <div key={index} className="mb-2 flex flex-col">
-                    <div className="flex items-center gap-3">
-                      <img
-                        src={profilePicture}
-                        className="size-10 ring-1 ring-white rounded-full"
-                      />
-                      <span className="font-bold text-sm text-gray-300">
-                        {msg.nickname}
-                      </span>
-                    </div>
-                    <span>{msg.message}</span>
-                  </div>
-                ))}
-              <div ref={messagesEndRef} />
-            </div>
-          </div>
+          <Log />
           <input
             className="w-full h-[10%] bg-transparent ring-1 rounded-md p-2 text-left align-top resize-none overflow-y-auto focus:outline-none text-white"
             value={message}
---
+++ Front/src/Components/Log.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { useWebSocket } from "../WebSocketProvider";
+import { useUser } from "../UserProvider";
+
+export default function Log() {
+  const [messageData, setMessageData] = useState([]);
+  const messagesEndRef = useRef(null);
+
+  const { profilePicture } = useUser();
+  const { ws } = useWebSocket();
+
+  // History - Message Updater.
+  useEffect(() => {
+    if (ws) {
+      const handleMessage = ({ data }) => {
+        const parsedData = JSON.parse(data);
+        if (parsedData.type === "history") {
+          setMessageData(parsedData.messages);
+        } else {
+          setMessageData((prevMessages) => [...prevMessages, parsedData.data]);
+        }
+      };
+
+      ws.addEventListener("message", handleMessage);
+
+      return () => {
+        ws.removeEventListener("message", handleMessage);
+      };
+    }
+  }, [ws]);
+
+  // Smooth Scroll
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messageData]);
+
+  return (
+    <div className="w-full h-[90%]">
+      <div className="p-4 text-white h-full overflow-y-auto scrollbar-thin scrollbar-thumb-slate-900 scrollbar-track-transparent">
+        {messageData
+          .filter((msg) => msg !== undefined)
+          .map((msg, index) => (
+            <div key={index} className="mb-2 flex flex-col">
+              <div className="flex items-center gap-3">
+                <img
+                  src={profilePicture}
+                  className="size-10 ring-1 ring-white rounded-full"
+                />
+                <span className="font-bold text-sm text-gray-300">
+                  {msg.nickname}
+                </span>
+              </div>
+              <span>{msg.message}</span>
+            </div>
+          ))}
+        <div ref={messagesEndRef} />
+      </div>
+    </div>
+  );
+}
