40531d9e9d0c3895bf8267c20aabc875bb7c6814
udK01 <dominik.muranyi@gmail.com>
2024-12-31 17:35:35 +0000

    Compartmentalised User.

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,13 +3,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { useWebSocket } from "./WebSocketProvider";
 import { useUser } from "./UserProvider";
 
-import EditUser from "./EditUser";
+import User from "./Components/User";
 
 export default function App() {
-  const { nickname, profilePicture } = useUser();
+  const { profilePicture } = useUser();
   const { ws } = useWebSocket();
 
-  const [edit, setEdit] = useState(false);
   const [message, setMessage] = useState("");
   const [messageData, setMessageData] = useState([]);
   const messagesEndRef = useRef(null);
@@ -56,23 +55,10 @@ export default function App() {
 
   return (
     <section className="h-screen bg-slate-600 flex flex-col justify-center items-center">
-      {edit && <EditUser ws={ws} setEdit={setEdit} />}
-
       <div className="flex w-[40%] h-[60%]">
         <div className="w-[30%] bg-slate-700 ring-4 ring-slate-800 rounded-md">
           <div className="w-full h-[90%]"></div>
-          <div
-            className="w-full h-[10%] flex items-center gap-2 bg-slate-800 rounded-md p-2 hover:bg-slate-900 hover:cursor-pointer"
-            onClick={() => setEdit(true)}
-          >
-            <img
-              src={profilePicture}
-              className="w-10 h-10 rounded-full ring-1 ring-white"
-            />
-            <div className="text-white text-ellipsis line-clamp-1 text-[20px] whitespace-nowrap overflow-hidden">
-              {nickname}
-            </div>
-          </div>
+          <User />
         </div>
         <div className="w-[70%] bg-slate-700 ring-4 ring-slate-800 rounded-md">
           <div className="w-full h-[90%]">
