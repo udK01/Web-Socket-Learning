200201c97969b7e6ecd66f76193841e80c95a10a
udK01 <dominik.muranyi@gmail.com>
2025-01-04 07:02:14 +0000

    Added automatic chatbox select on Edit.

--- Front/src/Components/SendMessage.jsx
+++ Front/src/Components/SendMessage.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { useWebSocket } from "../WebSocketProvider";
 
 export default function SendMessage({ reply, setReply, edit, setEdit }) {
   const [message, setMessage] = useState("");
+  const inputRef = useRef(null);
 
   const { ws } = useWebSocket();
+
+  // Automatically Select Input Field On Edit.
+  useEffect(() => {
+    if (edit && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [edit]);
 
   // Message Handler
   const handleSendMessage = () => {
@@ -42,6 +50,7 @@ export default function SendMessage({ reply, setReply, edit, setEdit }) {
 
   return (
     <input
+      ref={inputRef}
       className="w-full h-[10%] bg-transparent ring-1 rounded-md p-2 text-left align-top resize-none overflow-y-auto focus:outline-none text-white"
       value={message}
       onChange={(e) => setMessage(e.target.value)}
