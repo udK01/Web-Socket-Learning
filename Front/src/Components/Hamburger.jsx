554745ad89b97314f70c0fbc77e9f4655eb8228e
udK01 <dominik.muranyi@gmail.com>
2025-01-21 14:08:41 +0000

    Added Hamburger Icon.

---
+++ Front/src/Components/Hamburger.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Hamburger = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <button
+      className="relative flex flex-col justify-between w-8 h-8 group"
+      onClick={toggleMenu}
+      aria-label="Toggle Menu"
+    >
+      {/* Top bar */}
+      <span
+        className={`block h-1 w-full bg-tertiary_light dark:bg-tertiary rounded transition-transform duration-300 group-hover:bg-accent_light dark:group-hover:bg-accent ${
+          isOpen ? "rotate-45 translate-y-3.5" : ""
+        }`}
+      ></span>
+      {/* Middle bar */}
+      <span
+        className={`block h-1 w-full bg-tertiary_light dark:bg-tertiary rounded transition-all duration-300 group-hover:bg-accent_light dark:group-hover:bg-accent ${
+          isOpen ? "opacity-0" : ""
+        }`}
+      ></span>
+      {/* Bottom bar */}
+      <span
+        className={`block h-1 w-full bg-tertiary_light dark:bg-tertiary rounded transition-transform duration-300 group-hover:bg-accent_light dark:group-hover:bg-accent ${
+          isOpen ? "-rotate-45 -translate-y-3.5" : ""
+        }`}
+      ></span>
+    </button>
+  );
+};
+
+export default Hamburger;
--- Front/src/Components/Header.jsx
+++ Front/src/Components/Header.jsx
@@ -1,16 +1,25 @@
+import { useScreenContext } from "../Providers/ScreenProvider";
 import { useGroup } from "../Providers/GroupProvider";
 
-import GoogleAuth from "./GoogleAuth";
 import ThemeSwitcher from "./ThemeSwitcher";
+import GoogleAuth from "./GoogleAuth";
+import Hamburger from "./Hamburger";
 
 export default function Header() {
   const { selectedGroup } = useGroup();
+  const isSmallScreen = useScreenContext();
 
   return (
     <div className="w-full h-[80px] flex items-center bg-primary_light dark:bg-primary">
       <div className="w-[20%] flex justify-between items-center px-10">
-        <GoogleAuth />
-        <ThemeSwitcher />
+        {isSmallScreen ? (
+          <Hamburger />
+        ) : (
+          <>
+            <GoogleAuth />
+            <ThemeSwitcher />
+          </>
+        )}
       </div>
       <div className="w-[80%] text-[32px] line-clamp-1">
         {selectedGroup && selectedGroup.groupName}
